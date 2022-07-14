fedeb23735764df3bd3189634bf7e7f3cbffbafc
VIKRANT SINGH <vikrantdksingh518@gmail.com>
2022-07-14 22:05:21 +0530

    Creating Adapter class

--- app/src/main/java/com/vsapps/notesapp/MainActivity.kt
+++ app/src/main/java/com/vsapps/notesapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.vsapps.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val notesRecyclerView:RecyclerView = findViewById(R.id.notesRecyclerView)
+        notesRecyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
     }
 }
---
+++ app/src/main/java/com/vsapps/notesapp/NotesAdapter.kt
@@ -0,0 +1,23 @@
+package com.vsapps.notesapp
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesAdapter:RecyclerView.Adapter<NotesViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+class NotesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
