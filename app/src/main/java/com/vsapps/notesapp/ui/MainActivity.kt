24953dcd2beba7b4abf241f4c2717bfe6c30edb3
VIKRANT SINGH <vikrantdksingh518@gmail.com>
2022-07-14 22:55:57 +0530

    Sending dummy data in adapter class

--- app/src/main/java/com/vsapps/notesapp/ui/MainActivity.kt
+++ app/src/main/java/com/vsapps/notesapp/ui/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.vsapps.notesapp.R
+import com.vsapps.notesapp.adapter.NoteData
+import com.vsapps.notesapp.adapter.NotesAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,18 @@ class MainActivity : AppCompatActivity() {
 
         val notesRecyclerView:RecyclerView = findViewById(R.id.notesRecyclerView)
         notesRecyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+
+        val data = ArrayList<NoteData>()
+        for (i in 0 until 100) {
+            val noteData = NoteData(
+                "Title $i",
+                "Note $i"
+            )
+            data.add(noteData)
+        }
+
+        val adapter = NotesAdapter(data)
+        notesRecyclerView.adapter = adapter
+
     }
 }
